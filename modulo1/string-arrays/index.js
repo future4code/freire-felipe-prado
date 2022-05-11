c4538732be805bd60c4a7646cbed5c57a5de6537
Felipe Kaê Martins Prado <tarotalking@gmail.com>
2022-05-11 16:47:32 -0300

    exercicio arrays com desafio

--- modulo1/string-arrays/index.js
+++ modulo1/string-arrays/index.js
@@ -17,51 +17,48 @@
 // console.log(`O email ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
 
 // exercicio 2.
-let comidasPreferidas = ["lasanha", "pizza", "risoto", "hamburger", "strogonoff"]
+// let comidasPreferidas = ["lasanha", "pizza", "risoto", "hamburger", "strogonoff"]
 
 
-console.log(comidasPreferidas)
-console.log(
-    `Essas são as minhas comidas preferidas: 
-    ${comidasPreferidas[0]}
-    ${comidasPreferidas[1]}
-    ${comidasPreferidas[2]}
-    ${comidasPreferidas[3]}
-    ${comidasPreferidas[4]}
-    `)
+// console.log(comidasPreferidas)
+// console.log(
+//     `Essas são as minhas comidas preferidas: 
+//     ${comidasPreferidas[0]}
+//     ${comidasPreferidas[1]}
+//     ${comidasPreferidas[2]}
+//     ${comidasPreferidas[3]}
+//     ${comidasPreferidas[4]}
+//     `)
 
-let suaComida = prompt("qual sua comida preferida")
-comidasPreferidas[1] = suaComida
-console.log(comidasPreferidas)
+// let suaComida = prompt("qual sua comida preferida")
+// comidasPreferidas[1] = suaComida
+// console.log(comidasPreferidas)
 
-//exercicio 3.
+// //exercicio 3.
 
-const listaDeTarefas = []
-const t1 = prompt("cite 1 tarefa que você precisa fazer no seu dia")
-const t2 = prompt("cite outra tarefa que você precisa fazer no seu dia")
-const t3 = prompt("cite mais uma tarefa que você precisa fazer no seu dia")
-listaDeTarefas.push(t1)
-listaDeTarefas.push(t2)
-listaDeTarefas.push(t3)
+// const listaDeTarefas = []
+// const t1 = prompt("cite 1 tarefa que você precisa fazer no seu dia")
+// const t2 = prompt("cite outra tarefa que você precisa fazer no seu dia")
+// const t3 = prompt("cite mais uma tarefa que você precisa fazer no seu dia")
+// listaDeTarefas.push(t1)
+// listaDeTarefas.push(t2)
+// listaDeTarefas.push(t3)
 
-console.log(listaDeTarefas)
+// console.log(listaDeTarefas)
 
-let indice = prompt("digite o indice 0, 1 ou 2 da tarefa que você já realizou.")
+// let indice = prompt("digite o indice 0, 1 ou 2 da tarefa que você já realizou.")
 
-listaDeTarefas.splice(indice, 1)
-console.log(listaDeTarefas)
+// listaDeTarefas.splice(indice, 1)
+// console.log(listaDeTarefas)
 
+// desafio 1.
 
+const frase = "hoje vou ao cinema"
+const fraseListada = frase.split(" ")
+console.log(fraseListada)
 
+// 2.
 
-
-
-
-
-
-
-
-
-
-
-
+const listaFrutas  = ["Banana", "Morango", "Laranja", "Abacaxi", "Ameixa"]
+console.log(listaFrutas.indexOf("Abacaxi"))
+console.log(listaFrutas.length)
