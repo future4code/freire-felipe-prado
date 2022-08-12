8c7b3c3bda9f84fabedfac086edcab71a5e3a390
Felipe Kaê Martins Prado <tarotalking@gmail.com>
2022-08-12 19:32:38 -0300

    lista-ts

---
+++ modulo4/lista-typescript/index.ts
@@ -0,0 +1,30 @@
+let nome:string = ""
+let data:string = "25/08/1990"
+
+//  function nomeNascimento (dataNascimento:string): string {
+//     const dataNascimento = data.split(" ") ;
+//     return (`Olá, me chamo ${nome}, nasci no dia ${DIA} do mês de ${MES} do ano de ${ANO}`)
+//  }
+ 
+ // 2.
+ function myName (nome:string): void {
+    console.log(nome)
+ }
+
+ // 3.
+ enum GENERO {
+	ACAO="ação",
+	DRAMA="drama",
+	COMEDIA="comédia",
+	ROMANCE="romance",
+	TERROR="terror"
+}
+
+let filme: string | number | any | number
+
+function movie (
+    nome: "", 
+    ano: '', 
+    GENERO: "", 
+    pontuacao?: ''
+    ): any {}
